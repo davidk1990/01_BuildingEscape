20ea5d55822018eae708567c32e685af52cfa445
David Kennedy <davekennedy1990@gmail.com>
2017-01-11 10:19:20 +0000

    Built the level and have basic pressure pad working.

---
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingEscape.h"
+#include "PositionReporter.h"
+
+
+// Sets default values for this component's properties
+UPositionReporter::UPositionReporter()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReporter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	FString objectName = GetOwner()->GetName();
+	FVector objectPos = GetOwner()->GetActorTransform().GetLocation();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at position %s"), *objectName, *objectPos.ToString());
+	
+}
+
+
+// Called every frame
+void UPositionReporter::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
